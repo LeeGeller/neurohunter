bf042d80c8c6fac1475f1d047a01d47407759daa
Будник Кристина <ssspers92@gmail.com>
2026-08-10 13:16:45 +0300

    feat: add PostgreSQL support with async configuration and update Alembic settings

---
+++ services/analyzer/app/database/postgres.py
@@ -0,0 +1,29 @@
+"""Postgres database connection."""
+
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from app.config.settings import (
+    settings,
+)
+
+DATABASE_URL = (
+    f'postgresql+asyncpg://'
+    f'{settings.postgres_user}:'
+    f'{settings.postgres_password}@'
+    f'{settings.postgres_host}:'
+    f'{settings.postgres_port}'
+    f'{settings.postgres_db}'
+)
+
+engine = create_async_engine(DATABASE_URL)
+
+async_session = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
--- services/analyzer/migrations/env.py
+++ services/analyzer/migrations/env.py
@@ -1,9 +1,26 @@
-from logging.config import fileConfig
+from logging.config import (
+    fileConfig,
+)
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from alembic import (
+    context,
+)
+from sqlalchemy import (
+    pool,
+)
+from sqlalchemy.engine import (
+    Connection,
+)
+from sqlalchemy.ext.asyncio import (
+    async_engine_from_config,
+)
 
-from alembic import context
+from app.config.settings import (
+    settings,
+)
+from app.database.base import (
+    Base,
+)
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -14,11 +31,23 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+config.set_main_option(
+    'sqlalchemy.url',
+    (
+        f'postgresql+asyncpg://'
+        f'{settings.postgres_user}:'
+        f'{settings.postgres_password}@'
+        f'postgres:'
+        f'{settings.postgres_port}/'
+        f'{settings.postgres_db}'
+    )
+)
+
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -50,29 +79,44 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
-def run_migrations_online() -> None:
+def do_run_migrations(connection: Connection) -> None:
+    """Run migrations in 'online' mode."""
+
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+async def run_migrations_online() -> None:
     """Run migrations in 'online' mode.
 
     In this scenario we need to create an Engine
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+    connectable = async_engine_from_config(
+        config.get_section(
+            config.config_ini_section, {}
+        ),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
+    async with connectable.connect() as connection:
+        await connection.run_sync(
+            do_run_migrations,
         )
 
-        with context.begin_transaction():
-            context.run_migrations()
-
+        await connectable.dispose()
 
 if context.is_offline_mode():
     run_migrations_offline()
 else:
-    run_migrations_online()
+    import asyncio
+
+    asyncio.run(
+        run_migrations_online(),
+    )
